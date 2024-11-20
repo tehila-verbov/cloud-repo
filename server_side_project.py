1bcfd1428d7acfd8bd8205195f6393f04d4f078a
Tehila Verbov <tehila.hubara@gmail.com>
2024-11-20 20:44:10 +0200

    This is server-side file for saving in the server

---
+++ server_side_project.py
@@ -0,0 +1,19 @@
+import time
+from severityOption import severityOption
+
+path = "/var/log/syslog"
+severity_dict = {"timestamp": 0, severityOption.INFO._name_: 0, severityOption.WARN._name_: 0, severityOption.ERROR._name_: 0}
+
+with open(path, "r") as file:
+    for line in file:
+        words = line.split()
+        for word in words:
+            if word == severityOption.INFO._name_:
+                severity_dict[severityOption.INFO._name_] += 1
+            elif word == severityOption.WARN._name_:
+                severity_dict[severityOption.WARN._name_] += 1
+            elif word == severityOption.ERROR._name_:
+                severity_dict[severityOption.ERROR._name_] +=1
+
+severity_dict["timestamp"] = int(time.time())
+print(severity_dict)
