4b755dfdc441f129d3e469269fea5f48bcde7572
Tehila Verbov <tehila.hubara@gmail.com>
2024-11-21 16:04:05 +0200

    fixed according the comments

--- server_side_project.py
+++ server_side_project.py
@@ -2,7 +2,12 @@ import time
 from severityOption import severityOption
 
 path = "/var/log/syslog"
-severity_dict = {"timestamp": 0, severityOption.INFO._name_: 0, severityOption.WARN._name_: 0, severityOption.ERROR._name_: 0}
+severity_dict = {
+                    "timestamp": int, 
+                    severityOption.INFO._name_: 0, 
+                    severityOption.WARN._name_: 0, 
+                    severityOption.ERROR._name_: 0
+                 }
 
 with open(path, "r") as file:
     for line in file:
@@ -13,7 +18,7 @@ with open(path, "r") as file:
             elif word == severityOption.WARN._name_:
                 severity_dict[severityOption.WARN._name_] += 1
             elif word == severityOption.ERROR._name_:
-                severity_dict[severityOption.ERROR._name_] +=1
+                severity_dict[severityOption.ERROR._name_] += 1
 
 severity_dict["timestamp"] = int(time.time())
 print(severity_dict)
