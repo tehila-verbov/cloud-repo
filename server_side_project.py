2212513c8fe883abda140a181215906d53e84c79
Tehila Verbov <tehila.hubara@gmail.com>
2024-11-21 16:08:41 +0200

    fixed: search by index

--- server_side_project.py
+++ server_side_project.py
@@ -12,13 +12,12 @@ severity_dict = {
 with open(path, "r") as file:
     for line in file:
         words = line.split()
-        for word in words:
-            if word == severityOption.INFO._name_:
-                severity_dict[severityOption.INFO._name_] += 1
-            elif word == severityOption.WARN._name_:
-                severity_dict[severityOption.WARN._name_] += 1
-            elif word == severityOption.ERROR._name_:
-                severity_dict[severityOption.ERROR._name_] += 1
+        if words[5] == severityOption.INFO._name_:
+            severity_dict[severityOption.INFO._name_] += 1
+        elif words[5] == severityOption.WARN._name_:
+            severity_dict[severityOption.WARN._name_] += 1
+        elif words[5] == severityOption.ERROR._name_:
+            severity_dict[severityOption.ERROR._name_] += 1
 
 severity_dict["timestamp"] = int(time.time())
 print(severity_dict)
